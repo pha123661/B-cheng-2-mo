bde13eab6eb4da172aad69f4ba9310e2affb394e
Li Sheng-Wei <pha123661@gmail.com>
2022-06-20 20:40:16 +0800

    Update command prompt

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ try:
     img_name = argv[1]
 except IndexError:
     print("請把檔案直接拖放到執行檔上！")
+    input("按下Enter鍵繼續...")
 
 
 img = Image.open(img_name)
@@ -14,9 +15,8 @@ ori_size = img.size
 new_img = img.resize((32, 32))
 new_img = new_img.resize(ori_size)
 
-print(img_name, basename(img_name))
 new_img_name = basename(img_name).split('.')
 new_img_name = f"{''.join(new_img_name[:-1])}_low_res.{new_img_name[-1]}"
 new_img.save(new_img_name)
-print(f"檔案已儲存：{new_img_name}")
-input()
+print(f"檔案：{new_img_name} 已經被逼成惡魔了")
+input("按下Enter鍵繼續...")
